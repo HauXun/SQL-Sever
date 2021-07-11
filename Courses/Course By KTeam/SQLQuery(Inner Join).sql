df398ade6b462398fb1c8497b4fe70283e27e60e
HauXun <hautrieu65@gmail.com>
2021-07-11 19:41:49 +0700

    Additional Inner Join Technique

---
+++ Courses/Course By KTeam/SQLQuery(Inner Join).sql
@@ -0,0 +1,17 @@
+﻿USE HowKteam
+GO
+
+-- Inner Join kiểu cũ -> có thể sau này không còn được hổ trợ
+-- Mọi Join cần điều kiện
+SELECT * FROM dbo.GIAOVIEN, dbo.BOMON
+WHERE dbo.BOMON.MABM = dbo.GIAOVIEN.MABM
+
+-- Inner Join kiểu mới đúng chuẩn
+SELECT * FROM dbo.GIAOVIEN INNER JOIN dbo.BOMON
+ON BOMON.MABM = GIAOVIEN.MABM
+AND dbo.BOMON.TRUONGBM = ''
+GO
+
+-- Có thể viết tắt Inner Join -> Join
+SELECT * FROM dbo.KHOA JOIN dbo.BOMON
+ON BOMON.MAKHOA = KHOA.MAKHOA
