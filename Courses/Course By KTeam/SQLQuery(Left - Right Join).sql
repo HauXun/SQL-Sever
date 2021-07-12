52031351533e0101640046117b5c8b4fd1746ab0
HauXun <hautrieu65@gmail.com>
2021-07-12 21:25:20 +0700

    Additional Left - Right Join Technique

---
+++ Courses/Course By KTeam/SQLQuery(Left - Right Join).sql
@@ -0,0 +1,17 @@
+﻿USE HowKteam
+GO
+
+-- Join
+SELECT * FROM dbo.GIAOVIEN JOIN dbo.BOMON
+ON BOMON.MABM = GIAOVIEN.MABM
+
+-- Left Join
+-- Bảng bên phải nhập vào bảng bên trái
+-- Record nào bảng bên phải không có thì sẽ để null
+-- Record nào bảng bên trái không có thì không điền vào
+SELECT * FROM dbo.GIAOVIEN LEFT JOIN dbo.BOMON
+ON BOMON.MABM = GIAOVIEN.MABM
+
+-- Right Join
+SELECT * FROM dbo.GIAOVIEN RIGHT JOIN dbo.BOMON
+ON BOMON.MABM = GIAOVIEN.MABM
