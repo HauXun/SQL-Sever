c78bf9c684c01aad6c9ed4fdb3d4c8442fd266d3
HauXun <hautrieu65@gmail.com>
2021-07-13 20:41:41 +0700

    Additional Full Outer Join Technique

---
+++ Courses/Course By KTeam/SQLQuery(Full Outer Join).sql
@@ -0,0 +1,14 @@
+﻿USE HowKteam
+GO
+
+SELECT * FROM dbo.GIAOVIEN INNER JOIN dbo.BOMON
+ON BOMON.MABM = GIAOVIEN.MABM
+
+-- Full Outer Join
+-- Gom 2 bảng lại và có điều kiện
+-- Bên nào không có dữ liệu thì để null
+SELECT * FROM dbo.GIAOVIEN FULL OUTER JOIN dbo.BOMON
+ON BOMON.MABM = GIAOVIEN.MABM
+
+-- Cross Join là tổ hợp mỗi record của bảng A với all record của bảng B
+SELECT * FROM dbo.GIAOVIEN CROSS JOIN dbo.BOMON
