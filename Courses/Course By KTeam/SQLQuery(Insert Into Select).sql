a7e0df5565dc2935269096ef5609ce5df6ccbcb3
HauXun <hautrieu65@gmail.com>
2021-07-15 20:48:11 +0700

    Additional Insert Into Select Technique

---
+++ Courses/Course By KTeam/SQLQuery(Insert Into Select).sql
@@ -0,0 +1,12 @@
+﻿USE HowKteam
+GO
+
+-- Insert Into Select -> Copy dữ liệu vào bảng đã tồn tại
+SELECT * INTO CloneGV
+FROM dbo.GIAOVIEN
+WHERE 1 = 0
+
+INSERT INTO CloneGV
+SELECT * FROM dbo.GIAOVIEN
+
+SELECT * FROM dbo.CloneGV
