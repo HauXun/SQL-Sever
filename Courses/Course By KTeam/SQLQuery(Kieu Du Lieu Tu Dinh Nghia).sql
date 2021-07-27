81f5515bfb1125fae2b3ed4f492b901c2ce969c3
HauXun <hautrieu65@gmail.com>
2021-07-27 23:27:34 +0700

    Additional Self-Defined Data Type Technique

---
+++ Courses/Course By KTeam/SQLQuery(Kieu Du Lieu Tu Dinh Nghia).sql
@@ -0,0 +1,26 @@
+﻿USE HowKteam
+GO
+
+-- Kiểu dữ liệu tự định nghĩa
+-- EXEC sp_addtype 'Tên kiểu dữ liệu', 'Kiểu dữ liệu thực tế', 'Not null' (có hay không đều được)
+EXEC sys.sp_addtype @typename = 'NNAME', -- sysname
+                    @phystype = 'nvarchar(100)', -- sysname
+                    @nulltype = 'Not null',   -- varchar(8)
+                    @owner = NULL     -- sysname
+GO
+
+CREATE TABLE TestType
+(
+	Name NNAME,
+	Address NVARCHAR(500)
+)
+GO
+
+EXEC sys.sp_addtype @typename = 'NADDRESS', -- sysname
+                    @phystype = 'nvarchar(100)', -- sysname
+                    @nulltype = 'NULL',   -- varchar(8)
+                    @owner = NULL     -- sysname
+GO
+
+-- Xóa type
+EXEC sys.sp_droptype 'NNAME'
